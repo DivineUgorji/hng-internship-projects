58bc375d0e90727287aabefbca36745c5bc40254
Divine Ugorji <divineugorji37@gmail.com>
2025-01-31 10:54:11 +0100

    update date format ⏱

--- dev-profile-card/index.js
+++ dev-profile-card/index.js
@@ -1,6 +1,7 @@
 function getUTCDateTime() {
   const now = new Date();
-  const utcDateTime = now.toISOString().replace("T", " ").substring(0, 19); // Format: YYYY-MM-DD HH:MM:SS
+  // Format: YYYY-MM-DD HH:MM:SS
+  const utcDateTime = now.toISOString().replace("T", " ").substring(0, 19);
   return utcDateTime;
 }
 document.addEventListener("DOMContentLoaded", () => {
